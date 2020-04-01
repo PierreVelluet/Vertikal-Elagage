5a0bb3022e3dfab5e3326994c535ffbe16518ee6
PierreVelluet <velluetp@gmail.com>
2020-04-01 20:33:24 +0200

    hooks done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,59 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-class Contact extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: '',
-        phone: '',
-        email: '',
-        message: ''
-      }
-    }
+function Contact (){
+  const [inputName, setInputName] = useState('');
+  const [inputPhoneNumber, setInputPhoneNumber] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputMessage, setInputMessage] = useState('');
 
-    onNameChange(event) {
-      console.log(this.state.name)
-        this.setState({name: event.target.value})
-      }
-    
-      onEmailChange(event) {
-        this.setState({email: event.target.value})
-      }
-    
-      onMessageChange(event) {
-        this.setState({message: event.target.value})
-      }
-      onNumberChange(event) {
-        this.setState({phone: event.target.value})
-      }
-    
-    handleSubmit(event) {
-      console.log('button clicked')
-    }
-  
-  render() {
+
+  const handleSubmit = (event)=>{
+    console.log('button clicked')
+  }
+ 
    return(
     <div>
         <h4>N'hésitez pas à nous contacter pour un devis gratuit ou tout autres questions !</h4>
         <h4>Nous vous recontacterons dans les meilleurs délais !</h4>
     <form className='Form' noValidate autoComplete="off">
-      <TextField className='Input mb-3' onChange={this.onNameChange.bind(this)} id="outlined-basic" label="Nom" required variant="outlined" />
-      <TextField className='Input mb-3' onChange={this.onNumberChange.bind(this)} id="outlined-basic" label="Téléphone" required variant="outlined" />
-      <TextField className='Input mb-3' onChange={this.onEmailChange.bind(this)} id="outlined-basic" label="Email" required variant="outlined" />
       <TextField
-          onChange={this.onMessageChange.bind(this)}
+        className='Input mb-3'
+        onChange={event => {
+                const newName = event.target.value;
+                setInputName({newName});
+              }}
+        id="outlined-basic"
+        label="Nom"
+        required variant="outlined"
+        value={inputName}
+      />
+
+      <TextField
+        className='Input mb-3'
+        onChange={event => {
+                const newPhoneNumber = event.target.value;
+                setInputPhoneNumber({newPhoneNumber});
+              }}
+        id="outlined-basic"
+        label="Téléphone"
+        required variant="outlined"
+        value={inputPhoneNumber}
+          
+      />
+      <TextField
+        className='Input mb-3'
+        onChange={event => {
+                const newEmail = event.target.value;
+                setInputEmail({newEmail});
+              }}
+        id="outlined-basic"
+        label="Email"
+        required variant="outlined"
+        value={inputEmail}
+          
+      />
+      <TextField
           className='Input mb-3'
+          onChange={event => {
+                const newMessage = event.target.value;
+                setInputMessage({newMessage});
+              }}
           id="filled-multiline-static"
           label="Votre message"
           multiline
           rows="6"
           variant="outlined"
+          value={inputMessage}
         />
-        <Button onClick={this.handleSubmit} variant="contained" color="primary">Envoyer</Button>
+        <Button onClick={() => {}} variant="contained" color="primary">Envoyer</Button>
     </form>
 
         </div>
@@ -61,6 +77,6 @@ class Contact extends React.Component {
   }
   
     
-  }
+  
   
   export default Contact;
